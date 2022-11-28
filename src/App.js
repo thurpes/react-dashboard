49b0b93f193393263a6fad728523df762dbe3993
thurpes <babatope.ifesanya@polarisdigitech.com>
2022-11-28 09:37:15 +0100

    added bg img

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import './App.css';
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-import './App.css';
 import{Navbar, Footer, Sidebar,ThemeSettings} from './components';
 import{Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line} from './pages';
 import { useStateContext } from './contexts/ContextProvider';
